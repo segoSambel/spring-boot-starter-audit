9ae07d6f6a928be922f4d4c64ce6d3bf23ef08fb
segoSambel <habibhusain313@gmail.com>
2026-01-16 23:06:34 +0700

    feat: enable AOP proxy support in `AuditAutoConfiguration`

--- src/main/java/io/github/segosambel/audit/config/AuditAutoConfiguration.java
+++ src/main/java/io/github/segosambel/audit/config/AuditAutoConfiguration.java
@@ -8,8 +8,10 @@ import io.github.segosambel.audit.support.DefaultAuditContextResolver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AuditAutoConfiguration {
 
     /**
